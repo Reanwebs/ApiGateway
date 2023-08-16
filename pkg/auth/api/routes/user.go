ccb569994422ad470932fc892cb24b557d5b6ebc
Edwin Siby <124251172+Edwinsiby@users.noreply.github.com>
2023-08-16 18:35:51 +0530

    Edwin (#8)
    * added api and common directory
    
    * added models
    
    * added models

--- pkg/auth/api/routes/user.go
+++ pkg/auth/api/routes/user.go
@@ -1,11 +1,2 @@
 package routes
 
-import (
-	"gateway/pkg/auth/api/handlers"
-
-	"github.com/gin-gonic/gin"
-)
-
-func RegisterVideoRoutes(api *gin.RouterGroup, authHandler handlers.SignupHandler) {
-
-}
