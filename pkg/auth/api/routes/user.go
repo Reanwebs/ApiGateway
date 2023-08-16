ff3ac7176a1db6e1f60cd831584678e7cdd60af1
edwinsiby <edwinsibyrajakumary@gmail.com>
2023-08-16 18:30:13 +0530

    added models

--- pkg/auth/api/routes/user.go
+++ pkg/auth/api/routes/user.go
@@ -1,11 +1,1 @@
 package routes
-
-import (
-	"gateway/pkg/auth/api/handlers"
-
-	"github.com/gin-gonic/gin"
-)
-
-func RegisterVideoRoutes(api *gin.RouterGroup, authHandler handlers.SignupHandler) {
-
-}
