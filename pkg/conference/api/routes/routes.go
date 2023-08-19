aa30e3184237aacfa2bb423c0330ea7fc3f5c5fd
Edwin Siby <124251172+Edwinsiby@users.noreply.github.com>
2023-08-19 14:31:42 +0530

    added commands in /pkg/conferance/api/routes/routes.go for routes (#20)

---
+++ pkg/conference/api/routes/routes.go
@@ -0,0 +1,13 @@
+package routes
+
+//start conference models.start conference
+//join conference  models.join  conference
+//accept join request
+//decline join request
+//remove participant
+//togglecam
+//togglemic
+//toggleparticipantcam
+//toggleparticipantmic
+//end conference
+//leave conference
