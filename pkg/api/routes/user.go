45ae1301d42916491a47975eee64aa7166846b9f
Rohith ER <129362673+RohithER12@users.noreply.github.com>
2023-08-24 18:18:31 +0530

    Rohith conference (#29)
    * creating jwt tocken when login (#28)
    
    * uncommended midleware in using in routes

--- pkg/api/routes/user.go
+++ pkg/api/routes/user.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"gateway/pkg/api/handlers"
+	middleware "gateway/pkg/common/midleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,7 +16,7 @@ func UserRoutes(api *gin.RouterGroup, userHandler handlers.UserHandler, conferen
 
 	routes.POST("/healthCheck")
 	confernce := api.Group("/conference")
-	// confernce.Use(middleware.AuthenticateUser)
+	confernce.Use(middleware.AuthenticateUser)
 	confernce.POST("/startConference", conferenceHandler.StartConference)
 	confernce.POST("/joinConference", conferenceHandler.JoinConference)
 	confernce.POST("/acceptJoining", conferenceHandler.AcceptJoining)
